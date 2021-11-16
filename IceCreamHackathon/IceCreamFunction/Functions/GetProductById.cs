436cad96c961f499bc453dcbb40f038a9c317dab
Thomas Bach Andersen <xtban@energinet.dk>
2021-11-16 11:02:35 +0100

    Add solution and first Azure Function

---
+++ IceCreamHackathon/IceCreamFunction/Functions/GetProductById.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace IceCreamFunction.Functions
+{
+    public static class GetProductById
+    {
+        [Function("GetProductById")]
+        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req, FunctionContext executionContext)
+        {
+            var query = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
+            
+            
+            
+            var logger = executionContext.GetLogger("GetProductById");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+            
+        }
+    }
+}
